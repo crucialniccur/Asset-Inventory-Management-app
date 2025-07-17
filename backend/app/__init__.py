56fdcf420274800f4a47df20ef87731e3a2cbd86
Mati Joseph <crucialniccur@gmail.com>
2025-07-17 23:37:46 +0300

    Add Flask-Migrate to Pipfile, initialize SQLAlchemy in app, and implement role-based access decorator

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from app.models import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    db.init_app(app)
     return app
