13411954975d3191552ccbe4e7a11e84682463cd
Mati Joseph <crucialniccur@gmail.com>
2025-07-17 23:54:40 +0300

    Add initial application setup with dotenv loading and database configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from app.models import db
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
     db.init_app(app)
+    Migrate(app, db)
     return app
--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,9 @@
 import os
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://asset_admin_user:yoursecurepassword@localhost/asset_innit"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
