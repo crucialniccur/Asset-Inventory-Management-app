03402c2f88ff0a7771591dfa053a16af301f2405
Hagee <hagee.ktr@outlook.com>
2025-07-22 12:45:48 +0300

    create error handling on missing fields

--- backend/app/routes/employee_request.py
+++ backend/app/routes/employee_request.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from flask import request
+from flask import jsonify, request
 from models import Request, db
 from decorators import role_required
 
@@ -12,6 +12,9 @@ class SubmitAssetRequest(Resource):
         type = data.get("type")
         reason = data.get("reason") 
         urgency = data.get("urgency")
+
+        if not all([asset_name, type, reason, urgency]):
+            return jsonify({'error': 'Missing required fields'}), 400
 
         new_request = Request(**data)
         db.session.add(new_request)
