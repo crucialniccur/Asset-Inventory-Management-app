8c49cf7d15e8c96230e33322625052c6ba6ee000
Hagee <hagee.ktr@outlook.com>
2025-07-22 12:26:18 +0300

    change file names removing resource and change file paths removing redundant paths

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,8 +1,8 @@
 def init_routes(app):
-    from .home import home_bp   # 👈 add this line
+    from .home import home_bp
     from .auth import auth_bp
     from .assets import asset_bp
 
-    app.register_blueprint(home_bp)   # 👈 and this line
+    app.register_blueprint(home_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(asset_bp)
