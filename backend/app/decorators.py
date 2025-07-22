8c8e47de83bd462f95130e6446e8c296df6bc57e
Hagee <hagee.ktr@outlook.com>
2025-07-22 10:39:05 +0300

    refactor: modify lines in decorator.py and add new user.py

--- backend/app/decorators.py
+++ backend/app/decorators.py
@@ -2,34 +2,31 @@ from functools import wraps
 from flask import request, jsonify, g
 from app.models import User
 import jwt
-from app.utils import SECRET_KEY
+from config import SECRET_KEY
 
-def role_required(*allowed_roles):
+from models import db
+
+def role_required(*roles):
     def decorator(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
-            auth = request.headers.get("Authorization", "")
-            token = auth.split(" ")[1] if " " in auth else auth
+            token = None
+            if 'Authorization' in request.headers:
+                token = request.headers['Authorization'].split(" ")[1]
             if not token:
-                return {"message": "Missing token"}, 401
+                return jsonify({"message": "Token is missing"}), 401
             
             try:
                 payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-                user = User.query.get(payload["user_id"])
-
-                if not user:
-                    return  {"message": "User not found"}, 404
-                
-                if not user.has_role(*allowed_roles):
-                    return {"message": "Forbidden: insufficient permissions"}, 403
-                
-                g.current_user = user
-                return fn(*args, **kwargs)
+                user = db.session.get(User, payload['user_id'])
+                if not user or user.role.value not in roles:
+                    return jsonify({"message": "Access forbidden"}), 403
             
             except jwt.ExpiredSignatureError:
                 return {"message": "Token expired"}, 401
             except jwt.InvalidTokenError:
                 return {"message": "Invalid token"}, 401
-            
+
+            return fn(*args, **kwargs)            
         return wrapper
     return decorator
