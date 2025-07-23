b13cb75f3faae5d0a749d8f9bfe334a9c0639591
Mati Joseph <crucialniccur@gmail.com>
2025-07-23 20:39:17 +0300

    add cloudinary config in app/__init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
 from app.models import db, init_app_models
+from dotenv import load_dotenv
+import os
+import cloudinary
+
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    secure=True
+)
+
 
 def create_app():
     app = Flask(__name__)
@@ -24,13 +38,13 @@ def create_app():
     from app.routes.home import home_bp
     from app.routes.users import users_bp
     from app.routes.assets import asset_bp
-    from app.routes.categories import category_bp   
+    from app.routes.categories import category_bp
     from app.routes.admin_request import requests_bp
     from app.routes.allocation import asset_allocation_bp
     from app.routes.employee_request import employee_bp
     from app.routes.finance_request import finrequests_bp
     from app.routes.procurement_request import procurement_bp
-    
+
     app.register_blueprint(users_bp)
     app.register_blueprint(category_bp)
     app.register_blueprint(auth_bp)
@@ -41,6 +55,5 @@ def create_app():
     app.register_blueprint(procurement_bp)
     app.register_blueprint(employee_bp)
     app.register_blueprint(asset_allocation_bp)
-
 
     return app
