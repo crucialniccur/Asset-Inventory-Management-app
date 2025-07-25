57e662892f8e3e1b90b20547902980a7e1aea58c
faith <faith.henu@student.moringaschool.com>
2025-07-25 18:04:52 +0300

    created login and register

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; // ⬅️ import this
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
+import store from './redux/store'; // ⬅️ make sure this path points to your store
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>   {/* ⬅️ wrap App with Provider */}
+      <App />
+    </Provider>
+  </StrictMode>
+);
